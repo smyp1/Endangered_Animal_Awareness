6b58a5cd9540c85cf6840382c3ae4cc46c3294fd
smyp1 <pinesmickey@gmail.com>
2022-07-25 14:18:12 -0400

    created the rest of the controllers

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,4 +1,7 @@
 const Charity = require("../Models/Charities");
+const Continent = require("../Models/Continents");
+const Biome = require("../Models/Biomes");
+const Animal = require("../Models/Animals");
 
 const createCharity = async (req, res) => {
   try {
@@ -54,10 +57,188 @@ const deleteCharity = async (req, res) => {
   }
 };
 
+const createContinent = async (req, res) => {
+  try {
+    const continent = await new Continent(req.body);
+    await continent.save();
+    return res.status(201).json({ continent });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getAllContinents = async (req, res) => {
+  try {
+    const continent = await Continent.find();
+    return res.status(200).json({ continent });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getContinentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const continent = await Continent.findById();
+    if (continent) {
+      return res.status(200).json({ continent });
+    }
+    return res.status(404).send("Continent with that ID was not found.");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const updateContinent = async (req, res) => {
+  try {
+    const continent = await Continent.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(continent);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+const deleteContinent = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Continent.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send("Continent was deleted.");
+    }
+    throw new Error("Continent not found.");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const createBiome = async (req, res) => {
+  try {
+    const biome = await new Biome(req.body);
+    await biome.save();
+    return res.status(201).json({ biome });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+const getAllBiomes = async (req, res) => {
+  try {
+    const biome = await Biome.find();
+    return res.status(200).json({ biome });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getBiomeById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const biome = await Biome.findById();
+    if (biome) {
+      return res.status(200).json({ biome });
+    }
+    return res.status(404).send("Biome with that ID was not found.");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+const updateBiome = async (req, res) => {
+  try {
+    const biome = await Biome.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(biome);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+const deleteBiome = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Biome.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send("Biome was deleted.");
+    }
+    throw new Error("Biome not found.");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+const createAnimal = async (req, res) => {
+  try {
+    const animal = await new Animal(req.body);
+    await animal.save();
+    return res.status(201).json({ animal });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+const getAllAnimals = async (req, res) => {
+  try {
+    const animal = await Animal.find();
+    return res.status(200).json({ animal });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getAnimalById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const animal = await Animal.findById();
+    if (animal) {
+      return res.status(200).json({ animal });
+    }
+    return res.status(404).send("Animal with that ID was not found.");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+const updateAnimal = async (req, res) => {
+  try {
+    const animal = await Animal.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(animal);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+const deleteAnimal = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Animal.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send("Animal was deleted.");
+    }
+    throw new Error("Animal not found.");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 module.exports = {
   createCharity,
   getAllCharities,
   getCharityById,
   updateCharity,
   deleteCharity,
+  createContinent,
+  getAllContinents,
+  getContinentById,
+  updateContinent,
+  deleteContinent,
+  createBiome,
+  getAllBiomes,
+  getBiomeById,
+  updateBiome,
+  deleteBiome,
+  createAnimal,
+  getAllAnimals,
+  getAnimalById,
+  updateAnimal,
+  deleteAnimal,
 };
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,31 @@ const controllers = require("../Controllers");
 const router = Router();
 
 router.get("/", (req, res) => res.send("This is the root!"));
+//Routes for charities
 router.post("/charities", controllers.createCharity);
 router.get("/charities", controllers.getAllCharities);
 router.get("/charities/:id");
 router.put("/charities/:id", controllers.updateCharity);
 router.delete("/charities/:id", controllers.deleteCharity);
 
+//Routes for continents
+router.post("/continents", controllers.createContinent);
+router.get("/continents", controllers.getAllContinents);
+router.get("/continents/:id", controllers.getContinentById);
+router.put("/continents/:id", controllers.updateContinent);
+router.delete("/continents/:id", controllers.deleteContinent);
+
+//Routes for biomes
+router.post("/biomes", controllers.createBiome);
+router.get("/biomes", controllers.getAllBiomes);
+router.get("/biomes/:id", controllers.getBiomeById);
+router.put("/biomes/:id", controllers.updateBiome);
+router.delete("/biomes/:id", controllers.deleteBiome);
+
+//Routes for animals
+router.post("/animals", controllers.createAnimal);
+router.get("/animals", controllers.getAllAnimals);
+router.get("/animals/:id", controllers.getAnimalById);
+router.put("/animals/:id", controllers.updateAnimal);
+router.delete("/animals/:id", controllers.deleteAnimal);
 module.exports = router;
