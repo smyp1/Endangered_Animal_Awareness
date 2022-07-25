7bf394e039b48cf2e621b5237cf66b45bd9d7799
smyp1 <pinesmickey@gmail.com>
2022-07-25 15:12:59 -0400

    created continents inside Database

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const routes = require("./routes");
 const db = require("./db");
+const logger = require("morgan");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
+app.use(logger("dev"));
 app.use("/api", routes);
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
