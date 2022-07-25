48b1fddecd01b08083d8afd036a036acc5be0fb1
smyp1 <pinesmickey@gmail.com>
2022-07-25 12:23:14 -0400

    tested controllers

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@ const router = Router();
 
 router.get("/", (req, res) => res.send("This is the root!"));
 router.post("/charities", controllers.createCharity);
+router.get("/charities", controllers.getAllCharities);
+router.get("/charities/:id");
+router.put("/charities/:id", controllers.updateCharity);
+router.delete("/charities/:id", controllers.deleteCharity);
 
 module.exports = router;
