81e86b5ed651bf23237dfccf4ad76443b8af8484
smyp1 <pinesmickey@gmail.com>
2022-07-25 12:05:12 -0400

    charity controllers created

--- Controllers/index.js
+++ Controllers/index.js
@@ -9,5 +9,55 @@ const createCharity = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+const getAllCharities = async (req, res) => {
+  try {
+    const charity = await Charity.find();
+    return res.status(200).json({ charity });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
-module.exports = { createCharity };
+const getCharityById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const charity = await Charity.findById(id);
+    if (charity) {
+      return res.status(200).json({ charity });
+    }
+    return res(404).send("Charity with the specified ID does not exist.");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const updateCharity = async (req, res) => {
+  try {
+    const charity = await Charity.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(charity);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+const deleteCharity = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Charity.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send("Charity was deleted.");
+    }
+    throw new Error("Charity not found.");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createCharity,
+  getAllCharities,
+  getCharityById,
+  updateCharity,
+  deleteCharity,
+};
