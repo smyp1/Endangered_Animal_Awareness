6bf2329810d0057ac49c0721209508a50dd93fb2
smyp1 <pinesmickey@gmail.com>
2022-07-26 11:29:37 -0400

    set up the nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header></header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
