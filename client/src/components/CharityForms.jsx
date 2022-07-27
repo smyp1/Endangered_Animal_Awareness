08b040b550308df793940f7913ed3741f06906e4
smyp1 <pinesmickey@gmail.com>
2022-07-27 11:06:05 -0400

    ability to create works

--- client/src/components/CharityForms.jsx
+++ client/src/components/CharityForms.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 
 const CharityForms =()=>{
@@ -11,9 +12,10 @@ const initialState = {
   const handleChange = (e) => {
     setNewCharity({ ...newCharity, [e.target.id]: e.target.value });
   };
-  const handleSubmit = (event) => {
+  const handleSubmit = async(event) => {
     event.preventDefault();
-    console.log(newCharity);
+    let res = await axios.post("http://localhost:3001/api/charities", newCharity)
+    console.log(res);
     setNewCharity(initialState);
   };
 
@@ -25,7 +27,7 @@ return (
     <label htmlFor="link">Link:</label>
     <input type="text" id="link" onChange={handleChange} value={newCharity.link}></input>
     <label htmlFor="description">Description:</label>
-    <textarea id="description" cols="20" rows="10"vonChange={handleChange} value={newCharity.description}></textarea>
+    <textarea id="description" cols="20" rows="10" onChange={handleChange} value={newCharity.description}></textarea>
     <label htmlFor="image">Image:</label>
     <input type="text" id="image" onChange={handleChange} value={newCharity.image}></input>
     <button type="submit">Submit</button>
